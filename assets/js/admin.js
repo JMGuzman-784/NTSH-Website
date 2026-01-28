03dcf3a0fe47cef29df21db6a359ed22f3a0a3f3
Jose M Guzman <Josemguzman784@gmail.com>
2026-01-28 13:03:38 -0600

    Update admin.js

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,3 +1,12 @@
+document.addEventListener("DOMContentLoaded", async () => {
+  if (!window.supabaseClient) {
+    console.error("Supabase client not available");
+    return;
+  }
+
+  const supabaseClient = window.supabaseClient;
+
+
 const SUPABASE_URL = "https://lworwldpziimhmcavjju.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx3b3J3bGRwemlpbWhtY2F2amp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE5MTA4ODcsImV4cCI6MjA3NzQ4Njg4N30.Nf0vYb3-DEUgumWNi3hfV1M7Vu6guQE_gzob4Ee-lao";
 // ===== ADMIN: REVIEW ONLY =====
