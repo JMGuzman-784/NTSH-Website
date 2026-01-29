c552b53e8e1a83c89e08a397d5f883e754bd7fbf
Jose M Guzman <Josemguzman784@gmail.com>
2026-01-29 15:15:55 -0600

    Update supabase.js

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,10 +1,11 @@
 // /assets/js/supabase.js
 const SUPABASE_URL = "https://lworwldpziimhmcavjju.supabase.co";
-const SUPABASE_KEY = "sb_publishable_fnQZFa3JFPl8EWJBq1emLw_LsqPZYPP";
+const SUPABASE_PUBLIC_KEY = "sb_publishable_fnQZFa3JFPl8EWJBq1emLw_LsqPZYPP";
 
 if (!window.supabaseClient) {
   window.supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
-    SUPABASE_KEY
+    SUPABASE_PUBLIC_KEY
   );
+  console.log("[Supabase] client initialized");
 }
