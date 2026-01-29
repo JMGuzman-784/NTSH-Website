7377f83be28eb663ee299cddea035d5572b50bd6
Jose M Guzman <Josemguzman784@gmail.com>
2026-01-29 15:09:19 -0600

    Update viewer.js

--- assets/js/viewer.js
+++ assets/js/viewer.js
@@ -1,5 +1,8 @@
 // /assets/js/viewer.js
 window.NTSH = {
   role: sessionStorage.getItem("ntsh_role"),
-  user: sessionStorage.getItem("ntsh_user")
+  user: sessionStorage.getItem("ntsh_user"),
+  ready: true
 };
+
+console.log("[NTSH Phase A]", window.NTSH);
