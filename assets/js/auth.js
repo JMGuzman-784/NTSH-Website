62f6809195c5d35c991a9828d2406b230fcc72d2
Jose M Guzman <Josemguzman784@gmail.com>
2026-01-29 13:39:49 -0600

    Update auth.js

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,35 +1,3 @@
-
-// /assets/js/auth.js
-document.addEventListener("DOMContentLoaded", () => {
-  
-
-  if (window.supabaseClient) return;
-
-  const supabase = window.supabase.createClient(
-    supabaseUrl,
-    supabaseKey
-  );
-
-  window.supabaseClient = supabase;
-
-  supabase.auth.getSession().then(({ data }) => {
-    if (!data.session) return;
-
-    const user = data.session.user;
-
-    sessionStorage.setItem("ntsh_uid", user.id);
-    sessionStorage.setItem(
-      "ntsh_user",
-      user.user_metadata?.username || "viewer_001"
-    );
-    sessionStorage.setItem(
-      "ntsh_role",
-      user.user_metadata?.role || "viewer"
-    );
-  });
-});
-
-
 // /assets/js/auth.js
 document.addEventListener("DOMContentLoaded", () => {
   if (!window.supabase) {
@@ -40,8 +8,6 @@ document.addEventListener("DOMContentLoaded", () => {
   if (window.supabaseClient) return;
 
   const supabaseUrl = "https://lworwldpziimhmcavjju.supabase.co";
-
-  
   // ✅ USE YOUR NEW PUBLISHABLE KEY HERE
   const supabaseKey = "sb_publishable_fnQZFa3JFPl8EWJBq1emLw_LsqPZYPP";
 
