10e6fdec5d22bf8e6e205970143709aaf692a542
Jose M Guzman <Josemguzman784@gmail.com>
2026-01-29 15:59:33 -0600

    Update supabase.js

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -2,10 +2,7 @@
 const SUPABASE_URL = "https://lworwldpziimhmcavjju.supabase.co";
 const SUPABASE_PUBLIC_KEY = "sb_publishable_fnQZFa3JFPl8EWJBq1emLw_LsqPZYPP";
 
-if (!window.supabaseClient) {
-  window.supabaseClient = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_PUBLIC_KEY
-  );
-  console.log("[Supabase] client initialized");
-}
+window.supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
