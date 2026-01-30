ac708f7d6d5e4c66db5a3360ed4ec328ea1877c2
Jose M Guzman <Josemguzman784@gmail.com>
2026-01-29 18:35:37 -0600

    Delete assets/js/supabase.js

--- assets/js/supabase.js
+++
@@ -1,8 +0,0 @@
-// /assets/js/supabase.js
-const SUPABASE_URL = "https://lworwldpziimhmcavjju.supabase.co";
-const SUPABASE_PUBLIC_KEY = "sb_publishable_fnQZFa3JFPl8EWJBq1emLw_LsqPZYPP";
-
-window.supabaseClient = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
