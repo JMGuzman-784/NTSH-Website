74bc4349681f4c93048a1fd1dec2a7f023ecee7c
Jose M Guzman <Josemguzman784@gmail.com>
2026-01-31 00:13:59 -0600

    Update supabase-client.js

--- assets/js/supabase-client.js
+++ assets/js/supabase-client.js
@@ -1,10 +1,7 @@
-
 // assets/js/supabase-client.js
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const SUPABASE_URL = "https://lworwldpziimhmcavjju.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_fnQZFa3JFPl8EWJBq1emLw_LsqPZYPP";
-
-window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
-console.log("[Supabase] client ready");
+export const supabase = createClient(
+  "https://lworwldpziimhmcavjju.supabase.co",
+  "sb_publishable_fnQZFa3JFPl8EWJBq1emLw_LsqPZYPP"
+);
